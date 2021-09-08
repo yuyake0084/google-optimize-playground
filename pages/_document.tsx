e4ae0cf5b382bdeeb49602a1b9a1954041f8ed2f
yuyake0084 <yuyake0084@gmail.com>
2021-09-08 09:18:35 +0900

    ボタン追加

---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    const { NODE_ENV, GA_TRACKING_ID, OPTIMIZE_CONTAINER_ID  } = process.env
+    const isProd = NODE_ENV === 'production';
+
+    console.log(GA_TRACKING_ID)
+
+    return (
+      <Html>
+        <Head>
+          {!isProd && (
+            <Fragment>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', { 'optimize_id': '${OPTIMIZE_CONTAINER_ID}'});
+                  `
+                }}
+              />
+            </Fragment>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
