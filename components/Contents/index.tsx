cafb1894c45c5c468505b003b88b7cc8f12386be
yuyake0084 <yuyake0084@gmail.com>
2021-09-10 20:22:40 +0900

    google_optimizeを実行

--- components/Contents/index.tsx
+++ components/Contents/index.tsx
@@ -1,10 +1,20 @@
 import useGoogleOptimize from '@react-hook/google-optimize'
+import { useEffect } from 'react'
 
 import styles from '../../styles/Contents.module.css'
 import { A } from './A'
 import { B } from './B'
 import { C } from './C'
 import { Default } from './Default'
+
+declare global {
+  interface Window {
+    google_optimize: {
+      get: (experimentId: string) => number
+    }
+  }
+}
+
 
 export const Contents = () => {
   const experimentId = process.env.NEXT_PUBLIC_EXPERIMENT_ID ?? ''
@@ -13,7 +23,12 @@ export const Contents = () => {
     [A, B, C],
   )
 
-  console.log(ContentsVariant)
+  useEffect(() => {
+    const variant = window.google_optimize.get(experimentId);
+
+    console.log(ContentsVariant, variant)
+  }, [])
+  
 
   return (
     <div className={styles.container}>
